159749d26b98af336b77646cee2b0eda8dd8fb2a
arseny <arsenid2003@gmail.com>
2026-01-07 16:44:46 +0500

    Убран ненужный файл с объектом конфига

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,13 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import svgr from 'vite-plugin-svgr';
 
-// https://vite.dev/config/
-export default defineConfig({
+export const viteConfigObj = {
   plugins: [react(), svgr()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
     }
   }
-});
+};
+// https://vite.dev/config/
+export default defineConfig(viteConfigObj);
